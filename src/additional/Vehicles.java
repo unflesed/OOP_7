e1aaa08e1a0f58e969c0e24d82c52b453172fb23
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-10-19 20:52:37 +0300

    task_2

---
+++ src/additional/Vehicles.java
@@ -0,0 +1,24 @@
+package additional;
+
+public enum Vehicles {
+    BMW(1000), AUDI(1100);
+    double price;
+    String color = "Black";
+    Vehicles(double price){
+       this.price = price;
+    }
+    String getColor(){
+        return color;
+    }
+    @Override
+    public String toString() {
+        return this.name() +
+                " price= " + price +
+                ", color= " + getColor();
+    }
+
+    public static void main(String[] args) {
+        Vehicles vehicle = Vehicles.BMW;
+        System.out.println(BMW);
+    }
+}
